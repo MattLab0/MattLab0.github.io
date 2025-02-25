8b3764f06eba634c09310da6cee144d6e51f320d
MattLab0 <matt93.p@gmail.com>
2025-02-25 19:49:21 +0100

    Updated biomedical work images

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,6 +54,6 @@ const sr = ScrollReveal({
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
-sr.reveal('.certification__img', { interval: 200 }); 
+sr.reveal('.skills__data, .work__img, .contact__input, .details-text', { interval: 200 });
+sr.reveal('.certification__img, .details-text', { interval: 200 }); 
 
